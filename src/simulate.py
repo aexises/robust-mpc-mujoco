18e4c11aafd8edc2610d06559e836b49e01a5b63
daeron <daeron@Noutbuk-daeron.local>
2026-01-13 22:34:22 +0300

    PD control and actuators template

--- src/simulate.py
+++ src/simulate.py
@@ -1,32 +1,31 @@
 import mujoco 
 import numpy as np
 import mediapy as media
+#from controller import pd_controller
+def pd_controller(kp, kd, q, qdes, qvel):
+    return kp * (qdes - q) - kd * qvel
 
 #Downloading model
 model = mujoco.MjModel.from_xml_path("/Users/daeron/robust-mpc-mujoco/models/universal_robots_ur5e/ur5e.xml")
 data = mujoco.MjData(model)
-
-#Model description 4 me 
-nbody = model.nbody
-nv = model.nv
-nq = model.nq
-print(f'Number of bodies: {nbody}')
-print(f'Total number of DoFs in the model: {nv}')
-print(f'Total number of generalized coordinates: {nq}')
-print(f'Number of geoms in the scene: {model.ngeom}')
-print(f'Colors of each geom: \n {model.geom_rgba}')
+mujoco.mj_resetData(model, data) 
 
 #simulation
 data.qpos = np.array([0, 0, 0, 0, 0, 0])
+qdes = np.array([-1,-0.8,2,2,2.7,10.7])
+
 duration = 5
 framerate = 60
 
 frames = []
-
+history_q = []
 renderer = mujoco.Renderer(model, width=640, height=480)
 
 while data.time < duration:
-    data.ctrl = np.array([-0.6,-1.2,-2.1,0.7,0.7,0.7])
+    q = data.qpos.copy()
+    qvel = data.qvel.copy()
+    history_q.append(q)
+    data.ctrl = pd_controller(10, 5, q, qdes, qvel)
     mujoco.mj_step(model, data)
     if len(frames) < data.time * framerate:
         renderer.update_scene(data)
